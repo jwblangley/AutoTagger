5365859c8e5c7d2ddc7fa623ca7f1dddac9a7d9b
James <jwblangley@gmail.com>
2018-08-30 11:52:21 +0100

    Completed triad mixing algorithm

--- src/colorTools.js
+++ src/colorTools.js
@@ -34,8 +34,42 @@ function pickColorsFromImage(img, numCols) {
     cols[i] = canvas.getContext('2d')
       .getImageData(x, y, 1, 1).data;
   }
-  console.log(cols);
+  console.log(triadMixing(cols, 10, 0.2));
 }
+// Inspired by
+// http://devmag.org.za/2012/07/29/how-to-choose-colours-procedurally-algorithms
+function triadMixing(inputCols, numOutputs, greyControl) {
+  var outCols = [];
+  for (var i = 0; i < numOutputs; i++) {
+    var randomIndex = randBetween(0, 3);
+
+    var mixRatio0 =
+    (randomIndex === 0) ? Math.random() * greyControl : Math.random();
+
+    var mixRatio1 =
+    (randomIndex === 1) ? Math.random() * greyControl : Math.random();
+
+    var mixRatio2 =
+    (randomIndex === 2) ? Math.random() * greyControl : Math.random();
+
+    var sum = mixRatio0 + mixRatio1 + mixRatio2;
+
+    mixRatio0 /= sum;
+    mixRatio1 /= sum;
+    mixRatio2 /= sum;
+
+    outCols[i] = [
+      Math.floor(mixRatio0 * inputCols[0][0] + mixRatio1 * inputCols[1][0] + mixRatio2 * inputCols[2][0]),
+      Math.floor(mixRatio0 * inputCols[0][1] + mixRatio1 * inputCols[1][1] + mixRatio2 * inputCols[2][1]),
+      Math.floor(mixRatio0 * inputCols[0][2] + mixRatio1 * inputCols[1][2] + mixRatio2 * inputCols[2][2]),
+      255
+    ];
+  }
+
+  return outCols;
+}
+
+
 
 
 
